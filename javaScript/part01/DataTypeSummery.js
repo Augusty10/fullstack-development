8426ef0782b45bacaacb44d605e3d3438b374144
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-06-11 01:20:04 +0530

    Add DataTypeSummary.js file with primitive and reference type examples

---
+++ javaScript/part01/DataTypeSummery.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types : String , Number , Boolean , null , undefined  , Symbol 
+//    BigInt , 
+
+const Score = 100
+const ScroewValue = 100.3 
+const isLoggedIn = false 
+const outsidetemp = null
+let userEmail;    //undefined 
+
+const id=Symbol('123')
+
+const anotherId = Symbol('123')
+
+// const bignumber= 32151515151511151
+
+
+// 02 . refrence Type (Non-primitive )
+// Array , Objects , Functions 
+
+const heros =["Superman ", "Naagraj ","Shaktiman ", "Doga"];
+
+let myOBJ={
+    name:raj,
+    age:22,
+}
+
+
+const myFunction =function(){
+    console.log(" hello world ");
+    
+}
+
+console.log(typeof myFunction);
