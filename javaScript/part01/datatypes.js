63bdaa9a87ef7927fdcb0ff107290b0f0a99ba00
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-06-10 22:44:41 +0530

    Add initial JavaScript files for data types and printing examples

---
+++ javaScript/part01/datatypes.js
@@ -0,0 +1,25 @@
+" use strict " ;  // treat all js code as newer version 
+// alert("hello")   // we are using node js , not browser 
+
+console.log (3+3)
+
+
+// Number => 2 to power 53 
+// bigint 
+// string => " "
+// boolean => true / false 
+// null => stand alone value 
+// undefined => variable decalred but not value 
+// Symbol  => unique 
+
+// object 
+console.log(typeof "hitesh");
+console.log(typeof null);  // null is a Object 
+console.log(typeof undefined); // undefined is undefined data type 
+
+
+
+
+
+
+
---
+++ javaScript/part01/printing.js
@@ -0,0 +1,14 @@
+// print 
+
+console.log(" Hello ")
+
+// Let , const and Var
+const accountId = abc1234
+let accountEmail = "raj@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+console.table([ ])
+
+
+
