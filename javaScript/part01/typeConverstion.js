a99cf8773c1503d30c5a1acc38b60ce30ae2f8ba
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-06-11 00:54:37 +0530

    Add type conversion examples in JavaScript

---
+++ javaScript/part01/typeConverstion.js
@@ -0,0 +1,69 @@
+let score = 33
+
+// console.log(typeof score );
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);   // nan (not a number) // null is specifed for undefined 0 
+
+// " 33" -> 33
+// "33 abs" -> NAN 
+// " true " =>1 ; false - 0 
+
+let isLogedIn= true 
+
+let booleanIsLoogedIn = Boolean(isLogedIn)
+console.log(booleanIsLoogedIn);
+
+
+//  1 => true ; 0=> false 
+// "" => false 
+// "hitesh"  => true 
+
+
+
+//  convertaion operation 
+
+let value = 3 
+let negValue = -value 
+// console.log(negValue);
+
+
+let str1 = "hello "
+let str2 = "Raj"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(2+"1");
+// console.log("1"+2+2);
+// console.log(2+2+"1");
+
+
+// console.log(+true); //1
+// console.log(+"");
+
+let num1, num2, num3
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
