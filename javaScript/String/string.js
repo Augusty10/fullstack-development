c4eb98e616dd36c7f5ea60c698067f285ba82c49
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-06-11 02:02:49 +0530

    Remove outdated JavaScript files for data types, comparison, printing, and type conversion; add new files with updated content and structure

---
+++ javaScript/String/string.js
@@ -0,0 +1,21 @@
+const name = "hitesh"
+const repocount = 50
+
+console.log(`hello my name is ${name}and my repo count is ${repocount}`);
+
+const gamename = new String('raj sahu')
+
+console.log(gamename[0]);
+console.log(gamename__Proto__);
+
+
+const newStringONe ="     hitesh"
+console.log(newStringONe);
+console.log(newStringONe.trim());  //  remove extra Spaces 
+
+const url = "https://hitesh.com/";
+
+url.replace('')
+
+
+
