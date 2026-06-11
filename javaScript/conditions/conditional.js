adc1359930fd84bc185debf1e0ec1bf7582e3011
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-06-11 15:05:11 +0530

    Add conditional checks for number validation, boolean state, and array emptiness in JavaScript

---
+++ javaScript/conditions/conditional.js
@@ -0,0 +1,60 @@
+ // checking   if a number is greter than another number : 
+
+//  let num1 =5
+//  let num2 = 8
+
+//  if(num1 >num2){
+//         console.log(" num 1 is greter then num two"
+//         );  
+//  }else {
+//         console.log(" Nope , num is not greter ");
+
+//  }     
+
+
+
+
+//  let username = "chai "
+//  let anotherUsername = "chai "
+
+//  if(username === anotherUsername){
+//      console.log(" Pick Another username ");
+//  }else {
+//     console.log(" you can pick another username");
+    
+//  }
+
+
+ // checking if a variable is number or not 
+
+ let score = 44
+
+ if(typeof score === "number"){
+    console.log(" yep , this is number ");
+ }else{
+    // console.log(" No. that is not a number ");
+    
+ }
+
+ // check if boolean value is true or false 
+
+ let isTeaReady = false 
+
+ if(isTeaReady){
+    // console.log(" Tea is raedy ");
+ } else{
+    // console.log(" Tea  is not Ready ");
+    
+ }
+
+ // Checking if array is empty or  not :
+
+ let items = []
+console.log(items.length);
+
+ if(items.length === 0){
+    console.log(" Array is Empty ");
+ }else{
+    console.log(" Array is not empty ");
+ }
+
