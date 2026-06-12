c46b4e978195438309bffa9b138bcfe750c1b0d2
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-06-12 23:39:32 +0530

    Add initial implementation of course object with destructuring example

---
+++ javaScript/Objects/objects3.js
@@ -0,0 +1,20 @@
+// de structuring 
+
+const course ={
+    corsename : "js in hindi ",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+// course.courseInstructor
+
+const {courseInstructor: instructor }= course
+
+console.log(instructor);
+
+
+
+// API 
+
+
+
+
