748df78c87309331e577f02a8ad754802aaef552
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-06-16 12:00:36 +0530

    Add initial implementation of Counter App with HTML, CSS, and JavaScript files

---
+++ Projects/Counter App/script.js
@@ -0,0 +1,18 @@
+//  document.getElementById("count-el").innerText=5
+
+
+
+// intilize the count as 0n
+// listen for clicks on the increment button 
+// increment the count variables when the buttoon is Clicked 
+// chnage the count-el in the HTML to reflect the new count 
+
+function increment(){
+      console.log(" The button was clicked ");
+
+}
+
+
+
+
+
