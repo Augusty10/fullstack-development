858e1ae78534d88ab9fd0ba664914e8458d15a90
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-06-29 19:11:32 +0530

    Fix typos and improve formatting in JavaScript event listeners

--- javaScript/DomBom/Dom/sccript.js
+++ javaScript/DomBom/Dom/sccript.js
@@ -1,2 +1,59 @@
-// exampple 1 
+// Example 1 
+
+  document.getElementById("changeTextButton").addEventListener('click' , function (){
+      let paragraph = document.getElementById("myParagraph");
+     paragraph.textContent=" the paragraph is changed "      
+    
+  })
+
+  // example 2 
+
+  document.getElementById("highlightFirstCity").addEventListener('click', function(){
+    let citesList = document.getElementById("citiesList");
+    citesList.firstElementChild.classList.add("highlight");
+  })
+
+
+  // example 3 
+
+  document.getElementById("changeOrder").addEventListener('click',function(){
+    document.getElementById("coffeeType");
+    coffeeType.textContent ="Espresso";
+    coffeeType.style.backgraoundColor="brown";
+    coffeeType.style.padding = "5px" ;
+  })
+
+
+    // example 4
+    
+
+    document.getElementById("addNewItem").addEventListener('click', function (){
+        let newItem =document.createElement('list')
+        newItem.textContent ="Eggs"
+        document.getElementById("shoppingList").appendChild(newItem)
+    
+    })
+
+    // EXAMPLE 5
+    document.getElementById("removeLastTask").addEventListener('click', function(){
+       document.getElementById("taskList");
+       taskList.lastElementChild.remove();
+
+    })
+
+// example 6
+
+
+
+// example 7 
+
+// example 8
+
+// example 9 
+
+// example 10
+
+
+
+
 
